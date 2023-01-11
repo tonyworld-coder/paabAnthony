e3a0b0d6905bc217f83b959f4a92f3f8384a3e1b
tonyworld <68330362+tonyworld-coder@users.noreply.github.com>
2023-01-11 02:34:14 +0100

    Created a seperate css named userSettings.css for Client and Admin SettingsCompoent, moved the stylings for it that was in paab.css to userSettings.css

--- resources/views/client/settings.blade.php
+++ resources/views/client/settings.blade.php
@@ -13,6 +13,7 @@
         <link rel="stylesheet" href="{{ asset('css/materialize.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/paab.css') }}">
         <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/userSettings.css') }}">
         <link rel="stylesheet" href="{{ asset('fonts/material-icons.css') }}">
     </head>
     <body>
