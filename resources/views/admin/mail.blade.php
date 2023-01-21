29f95a38b16583290607f91e2ff1065c2603d149
tonyworld <68330362+tonyworld-coder@users.noreply.github.com>
2023-01-21 03:20:12 +0100

    Changed favicon and project brand name from Paab to White Coat Domain

--- resources/views/admin/mail.blade.php
+++ resources/views/admin/mail.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>PaaB - {{auth()->user()->firstname.' ' .auth()->user()->lastname ?? '' }} | Messages</title>
+        <title>White Coat Domain - {{auth()->user()->firstname.' ' .auth()->user()->lastname ?? '' }} | Messages</title>
         <link rel="shortcut icon" href="{{ asset('/media/img/logo.png') }}" type="image/x-icon">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap">
--- resources/views/client/support.blade.php
+++ resources/views/client/support.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PaaB: People as a Brand - Support</title>
+        <title>White Coat Domain: People as a Brand - Support</title>
         <link rel="shortcut icon" href="{{ asset('/media/img/logo.png') }}" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap">
